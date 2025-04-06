2b8b05d6fd0c691875502cf59670a8682c89f61d
Joshua Barrass <josh.barrass.work@gmail.com>
2025-04-06 16:53:16 +0100

    Rearrange header

--- tools/gen_bsect_h.sh
+++ tools/gen_bsect_h.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-echo "load_file_offset equ 0x$(awk '/load_file:/{getline; print $2}' bsect.lst)" > bsect.h
+echo "jboot_segment equ $(awk '/RELOCATION_SEGMENT equ/{print $4}' bsect.lst)" > bsect.h
+echo "load_file_offset equ 0x$(awk '/load_file:/{getline; print $2}' bsect.lst)" >> bsect.h
 echo "get_cluster_of_file_offset equ 0x$(awk '/get_cluster_of_file:/{getline; print $2}' bsect.lst)" >> bsect.h
-echo "jboot_segment equ $(awk '/RELOCATION_SEGMENT equ/{print $4}' bsect.lst)" >> bsect.h
